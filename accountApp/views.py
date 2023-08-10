b2fffbcd09d12d6c6a1751cd46da4239ad27e0cb
Pieces98 <yunbeomkim98@gmail.com>
2023-08-10 16:14:58 +0900

    django course ch.21 commit

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountApp.models import HelloWorld
 
@@ -10,12 +14,18 @@ from accountApp.models import HelloWorld
 def hello_world(request):
     if request.method == "POST":
         tmp = request.POST.get('hello_world_input')
+
         new_hello_world = HelloWorld()
         new_hello_world.text = tmp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
         return HttpResponseRedirect(reverse('accountApp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return render(request, 'accountApp/hello_world.html', context={'hell o_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountApp:hello_world')
+    template_name = 'accountApp/create.html'
