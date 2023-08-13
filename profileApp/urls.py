143392f2a85ee63603a662cf0a9cd0c1d03495d8
Pieces98 <yunbeomkim98@gmail.com>
2023-08-13 14:05:17 +0900

    django course ch.31 commit

--- profileApp/urls.py
+++ profileApp/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
 
+from profileApp.views import ProfileCreateView
 
 app_name = 'profileApp'
 
 urlpatterns = [
-
+    path('create/', ProfileCreateView.as_view(), name='create')
 ]
--- profileApp/views.py
+++ profileApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileApp.forms import ProfileCreationForm
+from profileApp.models import Profile
+
 
 # Create your views here.
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountApp:hello_world')
+    template_name = 'profileApp/create.html'
+
+    def form_valid(self, form):
+        tmp_profile = form.save(commit=False)
+        tmp_profile.user = self.request.user
+        tmp_profile.save()
+        return super().form_valid(form)
