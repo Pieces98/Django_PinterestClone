a25921c5a2dab59a3bc86936c48a51c0b482af61
Pieces98 <yunbeomkim98@gmail.com>
2023-08-16 09:43:29 +0900

    django course ch.43 commit

--- subscribeApp/urls.py
+++ subscribeApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from projectApp.views import ProjectListView, ProjectCreateView, ProjectDetailView
-from subscribeApp.views import SubscriptionView
+from subscribeApp.views import SubscriptionView, SubscriptionListView
 
 app_name = 'subscribeApp'
 
 urlpatterns = [
-    path('subscribe/', SubscriptionView.as_view(), name='subscribe')
+    path('subscribe/', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubscriptionListView.as_view(), name='list')
 ]
